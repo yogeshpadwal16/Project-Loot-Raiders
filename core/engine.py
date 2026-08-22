abec1c3a2b160d4a18cafa6c7b0d808cec6b3e02
Yogesh Padwal <yogesh.apadwal@gmail.com>
2026-08-22 21:29:58 +0530

    fix: restore resilient Flipkart selectors

--- core/engine.py
+++ core/engine.py
@@ -581,7 +581,7 @@ def scrape_product_details(url: str, driver=None) -> dict:
             if platform == "amazon":
                 selectors = ["#productTitle", "span#productTitle", ".qa-title-text"]
             elif platform == "flipkart":
-                selectors = [".VU-ZEg", "span.B_NuCI", "h1.yrwE28", "h1 span"]
+                selectors = ["h1.B_NuCI", "span.VU-ZEz", "span.VU-ZEg", "h1.yrwE28", "span._35KyD6", "span._2W9tVh", ".VU-ZEg"]
             elif platform == "myntra":
                 selectors = [".pdp-title", ".pdp-name"]
             elif platform == "ajio":
@@ -610,7 +610,7 @@ def scrape_product_details(url: str, driver=None) -> dict:
             if platform == "amazon":
                 selectors = [".a-price-whole", "span.a-price .a-offscreen", "#priceblock_ourprice", "#priceblock_dealprice", ".apexPriceToPay span.a-offscreen", ".a-color-price"]
             elif platform == "flipkart":
-                selectors = [".Nx9w7A", "._30jeq3", "div._30jeq3._16JkK1", "div.hlbKVd"]
+                selectors = ["div.Nx9bqj.CxhGGd", "div.Nx9bqj", "div._30jeq3._16Jk6d", "div._30jeq3", "div.hlbKVd"]
             elif platform == "myntra":
                 selectors = ["span.pdp-price strong", ".pdp-price"]
             elif platform == "ajio":
@@ -640,7 +640,7 @@ def scrape_product_details(url: str, driver=None) -> dict:
             if platform == "amazon":
                 selectors = ["span.a-price.a-text-price span.a-offscreen", "#listPrice", "#priceblock_listprice", "span.a-list-price"]
             elif platform == "flipkart":
-                selectors = [".y3NYbL", "._3I9_ww", "div._3I9_ww"]
+                selectors = ["div.yRaY8j._18RivS", "div.yRaY8j", "div._2p6JhP._30e3Er", "div._3I9_ww", "div._3AuQ35"]
             elif platform == "myntra":
                 selectors = ["span.pdp-mrp", ".pdp-mrp"]
             elif platform == "ajio":
@@ -709,7 +709,7 @@ def scrape_product_details(url: str, driver=None) -> dict:
             else:
                 selectors = []
                 if platform == "flipkart":
-                    selectors = ["img.DByoR4", "img._396cs4", "img.jfZQxf", "div.CXW8mj img"]
+                    selectors = ["img._396cs4", "img.DByuf4", "div._2r_T1I img", "img._53G4pf", "img.UCad5S", "img.vU5WPQ", "img.x1646t", "div.CXW8mj img"]
                 elif platform == "myntra":
                     selectors = ["img.pdp-image", ".image-grid-image"]
                 elif platform == "ajio":
